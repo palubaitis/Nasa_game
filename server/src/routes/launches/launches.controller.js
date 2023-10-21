edf5548273b378e46a6cd3a6e438837cf0debab8
Strike7777 <110612063+Strike7777@users.noreply.github.com>
2023-10-21 19:00:18 +0300

    Implemented form validation

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -6,7 +6,19 @@ function httpGetAllLaunches(req, res) {
 
 function httpAddNewLaunch(req, res) {
   const launch = req.body;
+
+if(!launch.mission || !launch.rocket || !launch.launchDate || !launch.destination){
+    return res.status(400).json({
+        error:"Missing require launch property"
+    })
+}
+
   launch.launchDate = new Date(launch.launchDate);
+  if( isNaN(launch.launchDate)) {
+    return res.status(400).json({
+        error:"Invalid launch date"
+    })
+  }
   console.log(launch);
   addNewLaunch(launch);
   return res.status(201).json(launch)
