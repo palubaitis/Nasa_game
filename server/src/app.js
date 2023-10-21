b1e7fdb91784720ec812bc401f9ca9b7920d72c1
Strike7777 <110612063+Strike7777@users.noreply.github.com>
2023-10-21 12:56:54 +0300

    .json files changes

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 const planetsRouter = require("./routes/planets/planets.router");
-
+const path = require("path")
 const app = express();
+const morgan = require("morgan")
 
 app.use(
   cors({
     origin: "http://localhost:3000",
   })
 );
+app.use(morgan("combined") )
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")))
 app.use(planetsRouter);
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname,"..", "public", "index.html" ))
+})
+
 
 module.exports = app;
